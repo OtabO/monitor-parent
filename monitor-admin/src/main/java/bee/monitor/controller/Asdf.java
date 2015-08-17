45065d49d3f15392ae089de4a91ac75b6cecb2c8
zhangsx <13611924659@126.com>
2015-08-17 23:42:49 +0800

    Initial commit

---
+++ monitor-admin/src/main/java/bee/monitor/controller/Asdf.java
@@ -0,0 +1,24 @@
+package bee.monitor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by zsx on 2015/8/17.
+ */
+@Controller
+@RequestMapping(value = "/asdf")
+public class Asdf {
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public ModelAndView list() {
+        ModelAndView mav = new ModelAndView("/asdf/list");
+
+        return mav;
+    }
+}
